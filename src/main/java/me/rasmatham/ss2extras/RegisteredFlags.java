01e11246efbe4c777e93f6eb2b847a086017973b
RasMatHam <rasmatham@gmail.com>
2024-02-19 16:44:22 +0100

    The island settings this mod adds should no longer change expected behaviour if the setting is not added to /plugins/SuperiorSkyblock2/menu/settings.yml

--- src/main/java/me/rasmatham/ss2extras/RegisteredFlags.java
+++ src/main/java/me/rasmatham/ss2extras/RegisteredFlags.java
@@ -2,8 +2,12 @@ package me.rasmatham.ss2extras;
 
 import com.bgsoftware.superiorskyblock.api.events.PluginInitializeEvent;
 import com.bgsoftware.superiorskyblock.api.island.IslandFlag;
+import me.rasmatham.ss2extras.SS2Extras;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+
+import java.util.Set;
+import java.util.logging.Level;
 
 public class RegisteredFlags implements Listener {
     public static IslandFlag FIREBALL;
@@ -13,19 +17,39 @@ public class RegisteredFlags implements Listener {
     public static IslandFlag VILLAGE_INVASION;
     public static IslandFlag VILLAGE_RAID;
 
-    @EventHandler
+    @EventHandler(ignoreCancelled = true)
     public void onPluginInit(PluginInitializeEvent e) {
-        IslandFlag.register("FIREBALL");
-        IslandFlag.register("PHANTOM_SPAWN");
-        IslandFlag.register("PILLAGER_PATROL");
-        IslandFlag.register("PORTAL_SPAWN");
-        IslandFlag.register("VILLAGE_INVASION");
-        IslandFlag.register("VILLAGE_RAID");
-        FIREBALL = IslandFlag.getByName("FIREBALL");
-        PHANTOM_SPAWN = IslandFlag.getByName("PHANTOM_SPAWN");
-        PILLAGER_PATROL = IslandFlag.getByName("PILLAGER_PATROL");
-        PORTAL_SPAWN = IslandFlag.getByName("PORTAL_SPAWN");
-        VILLAGE_INVASION = IslandFlag.getByName("VILLAGE_INVASION");
-        VILLAGE_RAID = IslandFlag.getByName("VILLAGE_RAID");
+        SS2Extras SS2E = SS2Extras.getInstance();
+        Set<String> availableIslandSettings = SS2E.getAvailableIslandSettings();
+        if (availableIslandSettings.contains("fireball")) {
+            IslandFlag.register("FIREBALL");
+            FIREBALL = IslandFlag.getByName("FIREBALL");
+            SS2E.getLogger().log(Level.INFO, "Flag enabled: FIREBALL");
+        }
+        if (availableIslandSettings.contains("phantom_spawn")) {
+            IslandFlag.register("PHANTOM_SPAWN");
+            PHANTOM_SPAWN = IslandFlag.getByName("PHANTOM_SPAWN");
+            SS2E.getLogger().log(Level.INFO, "Flag enabled: PHANTOM_SPAWN");
+        }
+        if (availableIslandSettings.contains("pillager_patrol")) {
+            IslandFlag.register("PILLAGER_PATROL");
+            PILLAGER_PATROL = IslandFlag.getByName("PILLAGER_PATROL");
+            SS2E.getLogger().log(Level.INFO, "Flag enabled: PILLAGER_PATROL");
+        }
+        if (availableIslandSettings.contains("portal_spawn")) {
+            IslandFlag.register("PORTAL_SPAWN");
+            PORTAL_SPAWN = IslandFlag.getByName("PORTAL_SPAWN");
+            SS2E.getLogger().log(Level.INFO, "Flag enabled: PORTAL_SPAWN");
+        }
+        if (availableIslandSettings.contains("village_invasion")) {
+            IslandFlag.register("VILLAGE_INVASION");
+            VILLAGE_INVASION = IslandFlag.getByName("VILLAGE_INVASION");
+            SS2E.getLogger().log(Level.INFO, "Flag enabled: VILLAGE_INVASION");
+        }
+        if (availableIslandSettings.contains("village_raid")) {
+            IslandFlag.register("VILLAGE_RAID");
+            VILLAGE_RAID = IslandFlag.getByName("VILLAGE_RAID");
+            SS2E.getLogger().log(Level.INFO, "Flag enabled: VILLAGE_RAID");
+        }
     }
 }
--- src/main/java/me/rasmatham/ss2extras/SS2Extras.java
+++ src/main/java/me/rasmatham/ss2extras/SS2Extras.java
@@ -2,21 +2,65 @@ package me.rasmatham.ss2extras;
 
 import me.rasmatham.ss2extras.flags.*;
 import org.bukkit.Server;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Objects;
+import java.util.Set;
+import java.util.logging.Level;
+
 public final class SS2Extras extends JavaPlugin {
+    private Set<String> availableIslandSettings;
+
+    private static SS2Extras instance = null;
     @Override
     public void onEnable() {
         // Plugin startup logic
+        instance = this;
+
+        File configFile = new File(SS2Extras.getInstance().getDataFolder(), "../SuperiorSkyblock2/menus/settings.yml");
+
+        FileConfiguration config = new YamlConfiguration();
+
+        try {
+            config.load(configFile);
+        } catch (IOException | InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
+
+        availableIslandSettings = Objects.requireNonNull(config.getConfigurationSection("settings")).getKeys(false);
+
         Server server = getServer();
         PluginManager pluginManager = server.getPluginManager();
         pluginManager.registerEvents(new RegisteredFlags(), this);
-        pluginManager.registerEvents(new Fireball(), this);
-        pluginManager.registerEvents(new PhantomSpawn(), this);
-        pluginManager.registerEvents(new PillagerPatrol(), this);
-        pluginManager.registerEvents(new PortalSpawn(), this);
-        pluginManager.registerEvents(new VillageInvasion(), this);
-        pluginManager.registerEvents(new VillageRaid(), this);
+        if (availableIslandSettings.contains("fireball")) {
+            pluginManager.registerEvents(new Fireball(), this);
+            }
+        if (availableIslandSettings.contains("phantom_spawn")) {
+            pluginManager.registerEvents(new PhantomSpawn(), this);
+        }
+        if (availableIslandSettings.contains("pillager_patrol")) {
+            pluginManager.registerEvents(new PillagerPatrol(), this);
+        }
+        if (availableIslandSettings.contains("portal_spawn")) {
+            pluginManager.registerEvents(new PortalSpawn(), this);
+        }
+        if (availableIslandSettings.contains("village_invasion")) {
+            pluginManager.registerEvents(new VillageInvasion(), this);
+        }
+        if (availableIslandSettings.contains("village_raid")) {
+            pluginManager.registerEvents(new VillageRaid(), this);
+        }
+    }
+    public static SS2Extras getInstance() {
+        return instance;
+    }
+    public Set<String> getAvailableIslandSettings() {
+        return this.availableIslandSettings;
     }
 }
